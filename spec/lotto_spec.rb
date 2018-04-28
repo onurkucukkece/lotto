b704e6b99a38af5f38bb3da281bb742e0a71efbd
onur <onurkucukkece@gmail.com>
2018-04-28 23:42:23 +0300

    added specs for lotto draw

--- spec/lotto_spec.rb
+++ spec/lotto_spec.rb
@@ -3,7 +3,16 @@ RSpec.describe Lotto do
     expect(Lotto::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "should pick specified amount of number from specified range" do
+    lotto = Lotto::Draw.new
+    expect(lotto.play({ pick: 6, of: 49 }).length).to eq(6)
+    100.times{ expect(lotto.play({ pick: 6, of: 49 }).sort.last).to be <= 49 }
+  end
+
+  it "should generate multilpe lotto columns" do
+    lotto = Lotto::Draw.new
+    columns = lotto.play({ pick: 6, of: 49, for: 5 })
+    expect(columns.length).to eq(5)
+    columns.each{ |col| expect(col.length).to eq(6) }
   end
 end
