4841fffb0fb6652379120d33ed0490921c4a2fa5
onur <onurkucukkece@gmail.com>
2018-04-28 23:42:05 +0300

    added draw class

---
+++ lib/lotto/draw.rb
@@ -0,0 +1,25 @@
+module Lotto
+  class Draw
+    def play(options)
+      @options = options
+      @options[:for].nil? ? draw : draw_multiple
+    end
+
+    def pick(drawns=[])
+      return rand(1..@options[:of]) if drawns.length == 0
+      (1..@options[:of]).reject{ |n| drawns.include? n }.sample
+    end
+
+    def draw
+      drawns = []
+      @options[:pick].times{ drawns << pick(drawns) }
+      drawns
+    end
+
+    def draw_multiple
+      coupons = []
+      @options[:for].times{ coupons << draw }
+      coupons
+    end
+  end
+end
