1ca47720b3666e4d0a0933ad8d462999620c616a
onur <onurkucukkece@gmail.com>
2018-05-02 01:12:06 +0300

    boyscouting

--- lib/lotto/draw.rb
+++ lib/lotto/draw.rb
@@ -1,24 +1,25 @@
 module Lotto
+  # lib/lotto/draw.rb
   class Draw
     def play(options)
       @options = options
       @options[:for].nil? ? draw : draw_multiple
     end
 
-    def pick(drawns=[])
-      return rand(1..@options[:of]) if drawns.length == 0
-      (1..@options[:of]).reject{ |n| drawns.include? n }.sample
+    def pick(drawns = [])
+      return rand(1..@options[:of]) if drawns.length.zero?
+      (1..@options[:of]).reject { |n| drawns.include? n }.sample
     end
 
     def draw
       drawns = []
-      @options[:pick].times{ drawns << pick(drawns) }
+      @options[:pick].times { drawns << pick(drawns) }
       drawns
     end
 
     def draw_multiple
       coupons = []
-      @options[:for].times{ coupons << draw }
+      @options[:for].times { coupons << draw }
       coupons
     end
   end
--- spec/lotto_spec.rb
+++ spec/lotto_spec.rb
@@ -1,38 +1,34 @@
 RSpec.describe Lotto do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Lotto::VERSION).not_to be nil
   end
 
   describe 'Draw' do
     let(:lotto) { Lotto::Draw.new }
     describe '#play' do
-      it "should pick specified amount of number from specified range" do
-        expect(lotto.play({ pick: 6, of: 49 }).length).to eq(6)
+      it 'should pick specified amount of number from specified range' do
+        expect(lotto.play(pick: 6, of: 49).length).to eq(6)
       end
 
-      it 'should pick a number once' do
-        100.times do
-          column = lotto.play({ pick: 6, of: 49 })
-          expect(column.uniq.length).to eq 6
+      100.times do
+        it 'should pick a number once' do
+          expect(lotto.play(pick: 6, of: 49).uniq.length).to eq 6
         end
-      end
 
-      it 'should pick within correct range' do
-        100.times do
-          column = lotto.play({ pick: 6, of: 49 })
-          expect(column.sort.last).to be <= 49
+        it 'should pick within correct range' do
+          expect(lotto.play(pick: 6, of: 49).max).to be <= 49
         end
       end
     end
 
     describe '#play' do
-      let(:columns) { lotto.play({ pick: 6, of: 49, for: 5 }) }
-      it "should generate multilpe lotto columns" do
+      let(:columns) { lotto.play(pick: 6, of: 49, for: 5) }
+      it 'should generate multilpe lotto columns' do
         expect(columns.length).to eq(5)
       end
 
       it 'should pick a number once in a colum' do
-        columns.each{ |col| expect(col.uniq.length).to eq(6) }
+        columns.each { |col| expect(col.uniq.length).to eq(6) }
       end
     end
   end
